6b7106321726f96daf550b2fc6511608aa1d5d6e
Mleschhorn <157720483+Mleschhorn@users.noreply.github.com>
2024-11-29 09:06:29 -0500

    Add files via upload

---
+++ Taller Swing Restaurantes Esqueleto/Taller Swing Restaurantes Esqueleto/src/uniandes/dpoo/swing/interfaz/principal/PanelBotones.java
@@ -0,0 +1,62 @@
+package uniandes.dpoo.swing.interfaz.principal;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelBotones extends JPanel implements ActionListener
+{
+    /**
+     * El comando para el botón para crear un nuevo restaurante
+     */
+    private static final String NUEVO = "nuevo";
+
+    /**
+     * El comando para el botón para ver todos los restaurantes en el mapa
+     */
+    private static final String VER = "ver";
+
+    private JButton butNuevo;
+    private JButton butVerTodos;
+    private VentanaPrincipal ventanaPrincipal;
+
+    public PanelBotones( VentanaPrincipal ventanaPrincipal )
+    {
+        this.ventanaPrincipal = ventanaPrincipal;
+
+        setLayout( new FlowLayout( ) );
+
+        // Agrega el botón para crear un nuevo restaurante
+        // TODO completar
+        JButton buttonNuevo = new JButton("Nuevo");
+        buttonNuevo.setActionCommand(NUEVO);
+        buttonNuevo.addActionListener(this);
+        add(buttonNuevo);
+        		
+
+        // Agrega el botón para ver todos los restaurantes
+        // TODO completar
+        JButton buttonVerTodos = new JButton("Ver Todos");
+        buttonVerTodos.setActionCommand(VER);
+        buttonVerTodos.addActionListener(this);
+        add(buttonVerTodos);
+    }
+
+    @Override
+    public void actionPerformed( ActionEvent e )
+    {
+        String comando = e.getActionCommand( );
+        if( comando.equals( NUEVO ) )
+        {
+            ventanaPrincipal.mostrarVetanaNuevoRestaurante( );
+        }
+        else if( comando.equals( VER ) )
+        {
+            ventanaPrincipal.mostrarVentanaMapa( );
+        }
+    }
+}
